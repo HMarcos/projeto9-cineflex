fd098bd73aed37fab8019c152f7070d75a67961e
Marcos Henrique <marcosmarcone48@gmail.com>
2022-03-25 14:49:06 -0300

    Configurações Iniciais do Projeto

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom";
+
+import App from "./componentes/App";
+
+import "./assets/css/reset.css";
+import "./assets/css/estilos.css"
+
+const divRoot = document.querySelector(".root");
+
+ReactDOM.render(<App />, divRoot);
